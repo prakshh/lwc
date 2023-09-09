e54bdeebdf212bec208efb53ba9ed555e5ed9ea7
prakshh <prakashdas18@gmail.com>
2023-09-09 11:27:51 +0530

    data binding and handling

--- todoManager/force-app/main/default/lwc/createTask/createTask.js
+++ todoManager/force-app/main/default/lwc/createTask/createTask.js
@@ -1,3 +1,18 @@
 import { LightningElement } from 'lwc';
 
-export default class CreateTask extends LightningElement {}
+export default class CreateTask extends LightningElement {
+    taskTitle;
+    dueDate;
+
+    handleOnChange(event) {
+        const fieldName = event.target.name;
+        if(fieldName === 'taskTitle') {
+            this.taskTitle = event.target.value;
+            console.log("this.taskTitle = ", this.taskTitle);
+        }
+        else if(fieldName === 'dueDate') {
+            this.dueDate = event.target.value;
+            console.log("this.dueDate = '", this.dueDate);
+        }
+    }
+}
