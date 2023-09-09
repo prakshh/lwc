43e070d833242a20e01addd698fe4ca70d9473db
prakshh <prakashdas18@gmail.com>
2023-09-09 13:52:28 +0530

    dynamic conditional rendering

--- todoManager/force-app/main/default/lwc/createTask/createTask.js
+++ todoManager/force-app/main/default/lwc/createTask/createTask.js
@@ -3,16 +3,22 @@ import { LightningElement } from 'lwc';
 export default class CreateTask extends LightningElement {
     taskTitle;
     dueDate;
+    showDueDate = false;
+    showSave = false;
 
     handleOnChange(event) {
         const fieldName = event.target.name;
         if(fieldName === 'taskTitle') {
             this.taskTitle = event.target.value;
-            console.log("this.taskTitle = ", this.taskTitle);
+            if(this.taskTitle != '') {
+                this.showDueDate = true;
+            } else {
+                this.showDueDate = false;
+            }
         }
         else if(fieldName === 'dueDate') {
             this.dueDate = event.target.value;
-            console.log("this.dueDate = '", this.dueDate);
+            this.dueDate != '' ? (this.showSave = true) : (this.showSave = false);
         }
     }
 }
