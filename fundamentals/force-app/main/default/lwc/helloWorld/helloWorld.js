5bf9e470d62e81049153a982710296a2819f18d0
prakshh <prakashdas18@gmail.com>
2024-02-20 23:54:04 +0530

    One way dataBinding - Controller to Template

--- fundamentals/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ fundamentals/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+    fullName = 'Zero to Hero'
+    animalSounds = {
+        'dog': 'woof',
+        'horse': 'neigh'
+    }
+}
