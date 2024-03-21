7d0f5b4b52579f691783986d8588285fcf6847ef
prakshh <prakashdas18@gmail.com>
2024-03-22 01:55:35 +0530

    displaying {quiz.id} - {quiz.question}

--- fundamentals/force-app/main/default/lwc/quizApp/quizApp.js
+++ fundamentals/force-app/main/default/lwc/quizApp/quizApp.js
@@ -1,6 +1,39 @@
 import { LightningElement } from 'lwc';
 
-export default class QuizApp extends LightningElement {}
+export default class QuizApp extends LightningElement {
+    myQuestions = [
+        {
+            id: "Question1",
+            question: "Which one of the following is not a template loop?",
+            answers:{
+                a: "for:each",
+                b: "iterator",
+                c: "map loop"
+            },
+            correctAnswer: "c"
+        },
+        {
+            id: "Question2",
+            question: "Which one of the following file type is invalid in LWC component folder?",
+            answers:{
+                a: ".svg",
+                b: ".apex",
+                c: ".js"
+            },
+            correctAnswer: "b"
+        },
+        {
+            id: "Question2",
+            question: "Which one of the following is not a directive?",
+            answers:{
+                a: "for:each",
+                b: "if:true",
+                c: "@track"
+            },
+            correctAnswer: "c"
+        }
+    ]
+}
 
 /*
 1. Component creation
