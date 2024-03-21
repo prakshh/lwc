ca3162bcb4f3019fa6f574e8db4539ed7f80d66d
prakshh <prakashdas18@gmail.com>
2024-03-21 23:54:34 +0530

    Quiz app - start

---
+++ fundamentals/force-app/main/default/lwc/quizApp/quizApp.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+
+export default class QuizApp extends LightningElement {}
+
+/*
+1. Component creation
+2. if:true directive
+3. Looping - for:each
+4. Getter
+5. Methods
+6. Properties
+7. Form submit and reset
+8. Html layout
+*/
