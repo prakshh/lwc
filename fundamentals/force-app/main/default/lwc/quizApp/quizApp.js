9e4cafcb88c9d1b717cee09ce3b3d0388b4aff55
prakshh <prakashdas18@gmail.com>
2024-03-22 18:46:19 +0530

    show number of correct answers & disable submit button if all answers not selected

--- fundamentals/force-app/main/default/lwc/quizApp/quizApp.js
+++ fundamentals/force-app/main/default/lwc/quizApp/quizApp.js
@@ -2,6 +2,7 @@ import { LightningElement } from 'lwc';
 
 export default class QuizApp extends LightningElement {
     selected = {}   //for storing answers
+    correctAnswers = 0  //number of cotrect answers
     myQuestions = [
         {
             id: "Question1",
@@ -24,7 +25,7 @@ export default class QuizApp extends LightningElement {
             correctAnswer: "b"
         },
         {
-            id: "Question2",
+            id: "Question3",
             question: "Which one of the following is not a directive?",
             answers:{
                 a: "for:each",
@@ -34,6 +35,12 @@ export default class QuizApp extends LightningElement {
             correctAnswer: "c"
         }
     ]
+
+    get allNotSelected() {
+        return !(Object.keys(this.selected).length === this.myQuestions.length)
+        //it is saying: if selected length != myQuestions length, then return true else return false
+        //it will keep the submit button disabled if all answers are not selected 
+    }
 
     changeHandler(event) {
         console.log("name", event.target.name)
@@ -45,9 +52,23 @@ export default class QuizApp extends LightningElement {
         this.selected = {...this.selected, [name]:value}    // it means - this.selected = new copy of this.selected with {"Question1":"a"}
     }
 
-    submitHandler() {}
+    submitHandler(event) {
+        event.preventDefault(); //it will prevent loading of page after clicking submit button
+        let correct = this.myQuestions.filter(item=>this.selected[item.id] === item.correctAnswer)
+        /*it means, this.selected = {"Question1":"a", "Question2":"b", "Question3":"c"}
+          selected contains user-selected values
+          correctAnswer contains correct values
+          filter will return all the questions which have correct answer as user-selected answer
+          it will return [{"id":"Question1","correctAnswer":"a"}]
+        */
+        this.correctAnswers = correct.length        //since filter (which is in 'correct') always returns array, we can use it to get the length of array, and hence, number of correct answers
+        console.log("this.correctAnswers", this.correctAnswers)
+    }
 
-    resetHandler() {}
+    resetHandler() {
+        this.selected = {}
+        this.correctAnswers = 0
+    }
 }
 
 /*
