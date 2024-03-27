248db437fe4f7551ee304a606ac731356b8a28bb
prakshh <prakashdas18@gmail.com>
2024-03-27 23:59:14 +0530

    setting style and attribute dynamically using querySelector

--- fundamentals/force-app/main/default/lwc/helloQuerySelectorDemo/helloQuerySelectorDemo.js
+++ fundamentals/force-app/main/default/lwc/helloQuerySelectorDemo/helloQuerySelectorDemo.js
@@ -4,11 +4,13 @@ export default class HelloQuerySelectorDemo extends LightningElement {
     userNames = ['Tony', 'Steve', 'Natasha', 'Wanda', 'Bruce']
     fetchDetailHandler() {
         const elem = this.template.querySelector('h1')
+        elem.style.border = "1px solid red"     //setting style dynamically using querySelector
         //console.log(elem)               // <h1>Hey, I'm a header</h1>
         console.log(elem.innerText)       // Hey, I'm a header
         const userElements = this.template.querySelectorAll('.name')
         Array.from(userElements).forEach(item=>{
             console.log(item.innerText)
+            item.setAttribute("title", item.innerText)      //setting attribute (adding title tag) dynamically using querySelector
         })
     }
 }
