012a7143e9eefca3a79e6a8adddd6ce0ec227c7f
prakshh <prakashdas18@gmail.com>
2024-03-28 10:12:13 +0530

    inline styling

---
+++ fundamentals/force-app/main/default/lwc/cssInLwc/cssInLwc.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+
+export default class CssInLwc extends LightningElement {}
+
+/*
+1. inline styling
+2. external css
+3. using Lightning Design System LDS
+4. SLDS Design Token
+5. Shared CSS in LWC
+6. Dynamic styling
+7. 3rd party CSS library
+8. Applying CSS across Shadow DOM
+*/ 
