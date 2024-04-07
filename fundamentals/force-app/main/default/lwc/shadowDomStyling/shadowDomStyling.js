9e521c4bbe8b85d21305235b375347e0ac75923f
prakshh <prakashdas18@gmail.com>
2024-04-07 20:10:22 +0530

    Shadow DOM Styling - its implementation using innerText

--- fundamentals/force-app/main/default/lwc/shadowDomStyling/shadowDomStyling.js
+++ fundamentals/force-app/main/default/lwc/shadowDomStyling/shadowDomStyling.js
@@ -1,6 +1,16 @@
 import { LightningElement } from 'lwc';
 
-export default class ShadowDomStyling extends LightningElement {}
+export default class ShadowDomStyling extends LightningElement {
+
+    renderedCallback() {
+        const style = document.createElement('style')
+        style.innerText = `c-shadow-dom-styling .slds-button {
+            background: red;
+            color: white;
+        }`
+        this.template.querySelector('lightning-button').appendChild(style)
+    }
+}
 
 /*
 
