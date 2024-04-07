8007a1e8112829e84399ff407907e455b7713a10
prakshh <prakashdas18@gmail.com>
2024-04-07 19:20:01 +0530

    Dynamic CSS - here, style="width:10%" is static. Let's make it Dynamic.

---
+++ fundamentals/force-app/main/default/lwc/dynamicCss/dynamicCss.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class DynamicCss extends LightningElement {
+    percent = 10
+
+    changeHandler(event) {
+        this.percent = event.target.value
+    }
+}
