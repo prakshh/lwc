14fb99e0caf18da40cc8a1a0fd8e0bc9c0f6874a
prakshh <prakashdas18@gmail.com>
2024-04-14 19:22:24 +0530

    render method - start

---
+++ fundamentals/force-app/main/default/lwc/renderMethod/renderMethod.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+import signinTemplate from './signinTemplate.html'
+import signupTemplate from './signupTemplate.html'
+import renderTemplate from './renderMethod.html'
+
+export default class RenderMethod extends LightningElement {
+
+    render() {
+        return renderTemplate
+    }
+}
+
+/*
+render method
+
+- can run before or after connectedCallback()
+- helps to render html conditonally
+*/
