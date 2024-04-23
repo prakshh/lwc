0e72698352edaffd8e96e9b7e8e1672923525905
prakshh <prakashdas18@gmail.com>
2024-04-23 10:04:02 +0530

    dynamic rendering

--- projects/todoManager/force-app/main/default/lwc/createTask/createTask.js
+++ projects/todoManager/force-app/main/default/lwc/createTask/createTask.js
@@ -3,6 +3,8 @@ import { LightningElement } from 'lwc';
 export default class CreateTask extends LightningElement {
     taskTitle;
     dueDate;
+    showDueDate = false;
+    showSave = false;
 
     handleOnChange(event) {
         //this.taskTitle = event.target.value;
@@ -10,9 +12,15 @@ export default class CreateTask extends LightningElement {
         if(fieldName === 'taskTitle') {
             this.taskTitle = event.target.value;
             console.log(" ~ this.taskTitle: ", this.taskTitle);
+            if(this.taskTitle != '') {
+                this.showDueDate = true;
+            } else {
+                this.showDueDate = false;
+            }
         } else if(fieldName === 'dueDate') {
             this.dueDate = event.target.value;
             console.log(" ~ this.dueDate: ", this.dueDate);
+            this.dueDate != '' ? (this.showSave = true) : (this.showSave = false);
         }
     }
 }
