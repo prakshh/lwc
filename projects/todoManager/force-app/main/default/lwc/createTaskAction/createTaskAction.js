474919a6d37eab62324480fcabd3314f2fccab25
prakshh <prakashdas18@gmail.com>
2024-04-25 01:05:23 +0530

    adding Target button

---
+++ projects/todoManager/force-app/main/default/lwc/createTaskAction/createTaskAction.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class CreateTaskAction extends LightningElement {}
+
+/*
+WE can use LWC to create a Target -> custom action (Quick Action).
+One LWC can have One Target.
+Quick Actions - are only available in lightning RecordPages, with target -> lightning__RecordAction
+Quick Actions are of 2 types - Screen Action and Headless Action
+*/
