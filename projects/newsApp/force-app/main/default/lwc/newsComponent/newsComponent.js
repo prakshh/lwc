b56d2621c4d810e9cddcfc70d1d8e2e3c3d7bb3a
prakshh <prakashdas18@gmail.com>
2024-06-19 21:21:38 +0530

    news controller and component

---
+++ projects/newsApp/force-app/main/default/lwc/newsComponent/newsComponent.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+import retrieveNews from "@salesforce/apex/NewsController.retrieveNews";
+
+export default class NewsComponent extends LightningElement {
+    connectedCallback() {
+        this.fetchNews();
+    }
+    fetchNews() {
+        retrieveNews().then(response=>{
+            console.log(response)
+        }).catch.error(error);
+    }
+}
